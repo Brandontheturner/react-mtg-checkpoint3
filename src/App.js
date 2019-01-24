7208f88e96406d2313a9e93f5623acde08e931cb
Brandon Turner <brandontheturner@gmail.com>
2019-01-23 22:42:32 -0600

    checkpoint 3 work

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null,
+      cardOffset: 40,
+      fetchedLimit: 20
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "http://api.magicthegathering.io/v1/cards/?pageSize=" +
+        this.state.fetchedLimits
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ data });
+      });
+  }
+
+  fetchCards = () => {
+    let url =
+      "http://api.magicthegathering.io/v1/cards/?pageSize=" +
+      this.state.cardOffset;
+    let cardOffset = (this.state.cardOffset += 20);
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({ data, cardOffset }));
+  };
+
   render() {
+    if (this.state.data === null) {
+      return <h1>Magic is happening</h1>;
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Magic The Gathering Reference Library</h1>
+        <div className="theCards">
+          {this.state.data.cards.map((card, i) => {
+            return (
+              <div>
+                <img key={i} src={card.imageUrl} alt="" />
+              </div>
+            );
+          })}
+        </div>
+        <button className="button" onClick={() => this.fetchCards()}>
+          More Cards
+        </button>
       </div>
     );
   }
 }
-
 export default App;
