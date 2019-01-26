55fd25ff7b0cd8107f603a53ab0b69a3399aebdc
Brandon Turner <brandontheturner@gmail.com>
2019-01-25 21:24:29 -0600

    styling page, layout, css adjustments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
+import Cards200 from "./Cards200";
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       data: null,
       cardOffset: 1,
       fetchedLimit: 1
     };
   }
-
   componentDidMount() {
-    fetch("http://api.magicthegathering.io/v1/cards/?pageSize=1")
+    fetch("http://api.magicthegathering.io/v1/cards/?page=150")
       .then(response => {
         return response.json();
       })
@@ -21,11 +20,10 @@ class App extends Component {
         this.setState({ data });
       });
   }
+  //create seperate fetch requests for each grouping of cards since there is a limit of 100 cards per page. have more cards button fetch the next 100 card grouping
 
   fetchCards = () => {
-    let url =
-      "http://api.magicthegathering.io/v1/cards/?pageSize=1" +
-      this.state.cardOffset;
+    let url = "http://api.magicthegathering.io/v1/cards/?page+1";
     let cardOffset = (this.state.cardOffset += 1);
 
     fetch(url)
@@ -35,11 +33,12 @@ class App extends Component {
 
   render() {
     if (this.state.data === null) {
-      return <h1>Magic is happening</h1>;
+      return <h1 className="loading">Gathering Magic</h1>;
     }
 
     return (
       <div className="App">
+        {this.state.Cards200}
         <h1>Magic The Gathering Reference Library</h1>
         <div className="theCards">
           {this.state.data.cards.map((card, i) => {
@@ -53,9 +52,35 @@ class App extends Component {
         <button className="button" onClick={() => this.fetchCards()}>
           More Cards
         </button>
-        <input type="text" placeholder="search" />
+        {/* <input type="text" placeholder="search" /> */}
+        {/* <div className="content">
+        <div className="container">
+          <section className="section">
+            <ul>
+              {this.state.cards.map(item => (
+                <li key={item}>{item}</li>
+                ))}
+          </ul>
+            </section>
+            <hr />
+            <section className="section">
+              <form className="form" id="addItemForm">
+                <input 
+                  type="text"
+                  className="input"
+                  id="addInput"
+                  placeholder="Find the card you are looking for..."
+                />
+                <button className="button is-info" onClick={this.addItem}>
+                Card Search
+                </button>
+              </form>
+            </section>
+        </div>
+      </div> */}
       </div>
     );
   }
 }
 export default App;
+// export default cards100;
---
+++ src/search.js
@@ -0,0 +1,5 @@
+class Search extends React.Component {
+  render() {
+    return <div />;
+  }
+}
