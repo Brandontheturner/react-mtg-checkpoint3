a16f0e7b44ded4ddf0d71134225930277fd36c69
Brandon Turner <brandontheturner@gmail.com>
2019-01-26 18:12:18 -0600

    styling, positioning, modal implementation, paganation

--- src/App.js
+++ src/App.js
@@ -1,86 +1,166 @@
 import React, { Component } from "react";
-import Cards200 from "./Cards200";
 import "./App.css";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null,
+      data: {},
       cardOffset: 1,
-      fetchedLimit: 1
+      fetchedLimit: 1,
+      cardPage: 2,
+      isLoading: true,
+      modal: false,
+      currenttype: "",
+      currentName: "",
+      currentmanaCost: "",
+      currentimageUrl: "",
+      currentrarity: "",
+      currentsetName: "",
+      currenttext: "",
+      currentpower: "",
+      currenttoughness: ""
     };
   }
   componentDidMount() {
-    fetch("http://api.magicthegathering.io/v1/cards/?page=150")
+    fetch("http://api.magicthegathering.io/v1/cards/?page=1")
       .then(response => {
         return response.json();
       })
-      .then(data => {
-        this.setState({ data });
-      });
+      .then(data => this.setState({ data }, () => this.dataLoaded()));
   }
+
+  dataLoaded = () => {
+    console.log("data loaded");
+    this.setState({ isLoading: false });
+  };
   //create seperate fetch requests for each grouping of cards since there is a limit of 100 cards per page. have more cards button fetch the next 100 card grouping
 
-  fetchCards = () => {
-    let url = "http://api.magicthegathering.io/v1/cards/?page+1";
-    let cardOffset = (this.state.cardOffset += 1);
+  fetchCards = page => {
+    console.log("fetchCards", page);
+
+    let url = "http://api.magicthegathering.io/v1/cards/?page=" + page;
+    // let cardOffset = (this.state.cardOffset += 1);
 
     fetch(url)
       .then(response => response.json())
-      .then(data => this.setState({ data, cardOffset }));
+      .then(data => this.setState({ data }));
+    this.setState({ cardPage: page + 1 });
+  };
+
+  toggleModal = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  };
+
+  cardData = card => {
+    this.setState({
+      currenttype: card.type,
+      currentName: card.name,
+      currentmanaCost: card.manaCost,
+      currentimageUrl: card.imageUrl,
+      currentrarity: card.rarity,
+      currentsetName: card.setName,
+      currenttext: card.text,
+      currentpower: card.power,
+      currenttoughness: card.toughness
+    });
+    this.toggleModal();
   };
 
   render() {
-    if (this.state.data === null) {
-      return <h1 className="loading">Gathering Magic</h1>;
+    if (this.state.isLoading) {
+      return (
+        <div className="App">
+          <h1>Magic The Gathering Reference Library</h1>
+          <IsLoading />
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <h1>Magic The Gathering Reference Library</h1>
+          <RenderCards
+            data={this.state.data}
+            page={this.state.cardPage}
+            fetchCards={this.fetchCards}
+            cardData={this.cardData}
+            toggleModal={this.toggleModal}
+            modal={this.state.modal}
+            currenttype={this.state.currenttype}
+            currentName={this.state.currentName}
+            currentmanaCost={this.state.currentmanaCost}
+            currentimageUrl={this.state.currentimageUrl}
+            currentrarity={this.state.currentrarity}
+            currentsetName={this.state.currentsetName}
+            currenttext={this.state.currenttext}
+            currentpower={this.state.currentpower}
+            currenttoughness={this.state.currenttoughness}
+          />
+        </div>
+      );
     }
-
-    return (
-      <div className="App">
-        {this.state.Cards200}
-        <h1>Magic The Gathering Reference Library</h1>
-        <div className="theCards">
-          {this.state.data.cards.map((card, i) => {
-            return (
-              <div className="cardImage">
-                <img key={i} src={card.imageUrl} alt="" />
-              </div>
-            );
-          })}
-        </div>
-        <button className="button" onClick={() => this.fetchCards()}>
-          More Cards
-        </button>
-        {/* <input type="text" placeholder="search" /> */}
-        {/* <div className="content">
-        <div className="container">
-          <section className="section">
-            <ul>
-              {this.state.cards.map(item => (
-                <li key={item}>{item}</li>
-                ))}
-          </ul>
-            </section>
-            <hr />
-            <section className="section">
-              <form className="form" id="addItemForm">
-                <input 
-                  type="text"
-                  className="input"
-                  id="addInput"
-                  placeholder="Find the card you are looking for..."
-                />
-                <button className="button is-info" onClick={this.addItem}>
-                Card Search
-                </button>
-              </form>
-            </section>
-        </div>
-      </div> */}
-      </div>
-    );
   }
 }
+
+const IsLoading = () => {
+  return <h1 className="loading">Gathering Magic....</h1>;
+};
+
+const RenderCards = props => {
+  // console.log(this.props.data);
+  return (
+    <div>
+      <div className="theCards">
+        {props.data.cards.map((card, i) => {
+          return (
+            <div className="cardImage">
+              <img
+                key={i}
+                src={card.imageUrl}
+                alt=""
+                onClick={() => props.cardData(card)}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <Button className="button" onClick={() => props.fetchCards(props.page)}>
+        More Cards
+      </Button>
+      <Modal
+        className="modalContainer"
+        isOpen={props.modal}
+        toggle={() => props.toggleModal()}
+      >
+        <ModalHeader toggle={() => props.toggleModal()}>
+          <div className="cardName"> {props.currentName}</div>
+        </ModalHeader>
+        <ModalBody className="modalContent">
+          {/* {props.currentmanaCost} */}
+          <img
+            className="modalImage"
+            src={props.currentimageUrl}
+            alt={props.currentName}
+          />
+          <div className="setName"> {props.currentsetName}</div>
+          {props.currentrarity}
+          {props.currenttext}
+          {props.currentpower}
+          {props.currenttoughness}
+          {props.currenttype}
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={() => props.toggleModal()}>
+            Done
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
 export default App;
 // export default cards100;
